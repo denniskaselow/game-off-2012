a2e88141ff0fa392177dd35918757f1571eaef22
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2012-12-24 09:08:10 +0100

    using a map to remember pressed keys

--- lib/src/html/input_systems.dart
+++ lib/src/html/input_systems.dart
@@ -12,11 +12,7 @@ class PlayerControlSystem extends PlayerStatusProcessingSystem {
   /** L. */
   const int LEAVE_LEVEL = 76;
 
-  bool accelerate = false;
-  bool turnLeft = false;
-  bool turnRight = false;
-  bool shoot = false;
-  bool leaveLevel = false;
+  Map<int, bool> keyPressed = new Map<int, bool>();
 
   num targetX = 0;
   num targetY = 0;
@@ -53,21 +49,21 @@ class PlayerControlSystem extends PlayerStatusProcessingSystem {
   }
 
   void processSystem() {
-    if (accelerate) {
+    if (keyPressed[ACCELERATE] == true) {
       velocity.x += 0.0025 * TrigUtil.cos(transform.angle);
       velocity.y += 0.0025 * TrigUtil.sin(transform.angle);
       spatial.resource = 'spaceship_thrusters.png';
     } else {
       spatial.resource = 'spaceship.png';
     }
-    if (turnLeft) {
+    if (keyPressed[LEFT] == true) {
       transform.angle = (transform.angle - 0.05) % FastMath.TWO_PI;
-    } else if(turnRight) {
+    } else if (keyPressed[RIGHT] ==  true) {
       transform.angle = (transform.angle + 0.05) % FastMath.TWO_PI;
     }
-    cannon.shoot = shoot;
-    if (leaveLevel) {
-      status.leaveLevel = leaveLevel;
+    cannon.shoot = keyPressed[SHOOT] == true;
+    if (keyPressed[LEAVE_LEVEL] == true) {
+      status.leaveLevel = keyPressed[LEAVE_LEVEL] == true;
       spatial.resource = 'spaceship.png';
       cannon.shoot = false;
       window.on.keyDown.remove(keyDownListener);
@@ -76,33 +72,11 @@ class PlayerControlSystem extends PlayerStatusProcessingSystem {
   }
 
   void handleKeyDown(KeyboardEvent e) {
-    int keyCode = e.keyCode;
-    if (keyCode == ACCELERATE) {
-      accelerate = true;
-    } else if (keyCode == LEFT) {
-      turnLeft = true;
-      turnRight = false;
-    } else if (keyCode == RIGHT) {
-      turnLeft = false;
-      turnRight = true;
-    } else if (keyCode == SHOOT) {
-      shoot = true;
-    } else if (keyCode == LEAVE_LEVEL) {
-      leaveLevel = true;
-    }
+    keyPressed[e.keyCode] = true;
   }
 
   void handleKeyUp(KeyboardEvent e) {
-    int keyCode = e.keyCode;
-    if (keyCode == ACCELERATE) {
-      accelerate = false;
-    } else if (keyCode == LEFT) {
-      turnLeft = false;
-    } else if (keyCode == RIGHT) {
-      turnRight = false;
-    } else if (keyCode == SHOOT) {
-      shoot = false;
-    }
+    keyPressed[e.keyCode] = false;
   }
 
   bool checkProcessing() => status.health > 0 && !status.leaveLevel && !status.enterLevel;
